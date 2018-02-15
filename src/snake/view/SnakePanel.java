7a4b3e08b10b32c18efba4ff888fee7752ecdc43
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-02-15 12:04:18 -0700

    Created setupPanel, setupLayout, and setupListeners

--- src/snake/view/SnakePanel.java
+++ src/snake/view/SnakePanel.java
@@ -15,15 +15,40 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 import javax.swing.Timer;
 
 public class SnakePanel extends JPanel 
 {
 	private SnakeController snakeController;
+	private SpringLayout appLayout;
 	
 	public SnakePanel(SnakeController snakeController)
 	{
 		super();
 		this.snakeController = snakeController;
+		
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	
 }
